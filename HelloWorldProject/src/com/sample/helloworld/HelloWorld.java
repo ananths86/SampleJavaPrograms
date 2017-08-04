b7000b8e60f7758851c97fa934939f355906bb9f
Sivaramakrishnan, Ananth <ananthcharm@gmail.com>
2017-08-04 18:28:25 +0530

    My Hello World Code !!

---
+++ HelloWorldProject/src/com/sample/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sample.helloworld;
+
+/**
+ * @author ananths
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("******Printing Hello World***********");
+		for(int i=0; i<10; i++) {
+			System.out.println("---Hello World----");
+		}
+	}
+
+}
